8c9fbdba81cc2c4aa5858ee119b47e894d852ac1
forgreatness <danh1nguyen23@live.com>
2020-03-12 18:41:23 -0700

    Updates

---
+++ precache-manifest.440c456d0ac2c2a8c4e765af0b6c6251.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48cc546c0b9ef1c54ead9a2f14352da",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "b70a75b3892f327d8562",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "015d75d7529a40ee504d",
+    "url": "/react-tutorial/static/js/2.22b8f73d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-tutorial/static/js/2.22b8f73d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b70a75b3892f327d8562",
+    "url": "/react-tutorial/static/js/main.5d4225a2.chunk.js"
+  },
+  {
+    "revision": "1b20ea05cd43bdedcf8b",
+    "url": "/react-tutorial/static/js/runtime-main.59791939.js"
+  }
+]);
